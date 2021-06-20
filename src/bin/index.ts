79701c96ca43093231f001f08d67e2211052594e
Manik Bajaj <manik@pixelconception.com>
2021-06-20 14:52:26 +0530

    Clear function working with mocdoc

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import clear from 'clear';
 import chalk from 'chalk';
 import figlet from 'figlet';
 const program = new Command();
 
+clear();
 console.log(chalk.yellow(figlet.textSync('MocDoc', { horizontalLayout: 'full' })));
